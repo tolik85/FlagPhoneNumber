fb844a257c6ee4ec8a1387f6fffb541ad9dc89af
Nathan Gurfinkel <tolik85@gmail.com>
2021-11-17 11:33:45 +0200

    xcode13 fix

--- Sources/FPNCountry.swift
+++ Sources/FPNCountry.swift
@@ -12,10 +12,10 @@ public struct FPNCountry: Equatable {
 		self.phoneCode = phoneCode
 		self.code = FPNCountryCode(rawValue: code)!
 
-		if let flag = UIImage(named: code, in: Bundle.FlagIcons, compatibleWith: nil) {
+		if let flag = UIImage(named: code) {
 			self.flag = flag
 		} else {
-			self.flag = UIImage(named: "unknown", in: Bundle.FlagIcons, compatibleWith: nil)
+			self.flag = UIImage(named: "unknown")
 		}
 	}
 
